616cebf06c2e1a60747535d4ccb9d5a23c805f6c
Alison Monteiro <alisonmonteiro.10@gmail.com>
2016-12-14 00:04:25 -0300

    creating a project according with the definition
    we can run `frontpress {new, --new or -n} project-name` and it will create
    
    project-name/
      |-- index.html

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
 'use strict';
 
+const fs = require('fs');
 const spawn = require('child_process').spawn;
+const slugify = require('slug');
+const inquirer = require('inquirer');
 
-module.exports.new = name => spawn('mkdir', [name]);
+function willReplacePath() {
+  const questions = [{
+    type: 'confirm',
+    name: 'replace',
+    message: 'You alredy have a project with this name. Replace project?',
+    default: false
+  }];
+
+  return inquirer.prompt(questions);
+}
+
+function createPath(name) {
+  return {
+    mkdir: spawn('mkdir', [name]),
+    touch: spawn('touch', [`${name}/index.html`]),
+    cd: spawn('cd', [name])
+  };
+}
+
+function newProject(name) {
+  const replace = willReplacePath;
+
+  return new Promise((resolve, reject) => {
+    name = slugify(name);
+
+    fs.exists(name, exists => {
+      if (exists) {
+        // TODO: improve tests for this.
+        return replace().then(res => res.replace ? resolve(createPath(name + 1)) : reject(false));
+      }
+
+      return resolve(createPath(name));
+    });
+  });
+}
+
+module.exports.new = newProject;
--- test.js
+++ test.js
@@ -1,12 +1,17 @@
 import test from 'ava';
 import fn from './';
 
-test('create a new project', t => {
+test('create a new project', async t => {
   const name = 'test';
-  const project = fn.new(name);
+  const project = await fn.new(name);
 
   t.is('object', typeof project, 'project is an object');
-  t.is('ChildProcess', project.constructor.name, 'project is an instance of `ChildProcess`');
-  t.is('object', typeof project.spawnargs, 'spawn args should be an object - array like');
-  t.true(project.spawnargs.indexOf(name) !== -1, 'should have the project name on spawn args');
+  t.is('ChildProcess', project.mkdir.constructor.name, 'command mkdir is an instance of `ChildProcess`');
+  t.is('ChildProcess', project.touch.constructor.name, 'command touch is an instance of `ChildProcess`');
+  t.is('ChildProcess', project.cd.constructor.name, 'command cd is an instance of `ChildProcess`');
+  t.is('object', typeof project.mkdir.spawnargs, 'spawn args should be an object - array like');
+  t.is('object', typeof project.cd.spawnargs, 'spawn args should be an object - array like');
+  t.true(project.mkdir.spawnargs.indexOf(name) !== -1, 'should have the project name on spawn args');
+  t.true(project.touch.spawnargs.indexOf(`${name}/index.html`) !== -1, 'should have an index.html inside the projec\'s path on spawn args');
+  t.true(project.cd.spawnargs.indexOf(name) !== -1, 'should have the project name on spawn args');
 });

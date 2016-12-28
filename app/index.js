ddb9a93cf541dd016244f914448a8fd6f912ac7d
Alison Monteiro <alisonmonteiro.10@gmail.com>
2016-12-27 23:11:37 -0300

    keep CLI methods on `cli.js`

--- app/index.js
+++ app/index.js
@@ -3,17 +3,22 @@
 const fs = require('fs');
 const childProcess = require('child_process');
 const slugify = require('slug');
-const inquirer = require('inquirer');
-const questions = require('./questions');
-
 const spawn = childProcess.spawn;
 const exec = childProcess.exec;
 
-function willReplacePath() {
-  return inquirer.prompt(questions.replaceProject);
+function checkNameIsUnique(name) {
+  name = slugify(name);
+
+  if (name === '') {
+    throw new Error(`You need to provide a project name.`);
+  }
+
+  return new Promise(resolve => {
+    fs.exists(name, exists => resolve(!exists));
+  });
 }
 
-function createProjectPath(name) {
+function newProject(name) {
   return {
     mkdir: spawn('mkdir', [name]),
     touch: spawn('touch', [`${name}/index.html`]),
@@ -21,38 +26,16 @@ function createProjectPath(name) {
   };
 }
 
-function newProject(name) {
-  const replace = willReplacePath;
-
-  return new Promise((resolve, reject) => {
-    name = slugify(name);
-
-    fs.exists(name, exists => {
-      if (exists) {
-        // TODO: improve tests for this.
-        return replace().then(res => {
-          if (res.replace) {
-            return resolve(createProjectPath(name))
-          }
-
-          return reject(Error(`Couldn't replace the prjoect path`))
-        });
-      }
-
-      return resolve(createProjectPath(name));
-    });
-  });
-}
-
 function init(answers) {
   const content = JSON.stringify(answers, null, 2);
 
-  fs.writeFile('frontpress.json', content, err => {
-    if (err) {
-      throw err;
+  fs.writeFile('frontpress.json', content, error => {
+    if (error) {
+      throw error;
     }
   });
 }
 
 module.exports.new = newProject;
 module.exports.init = init;
+module.exports.checkNameIsUnique = checkNameIsUnique;
--- cli.js
+++ cli.js
@@ -1,23 +1,44 @@
 #!/usr/bin/env node
 'use strict';
 
-const program = require('commander');
+const project = require('commander');
 const inquirer = require('inquirer');
-const frontpress = require('./app');
+
+const app = require('./app');
 const questions = require('./app/questions');
 
-program
+project
   .version('0.0.0')
   .option('-n, --new <name>', 'create a new project')
   .option('-i, --init', 'init the project')
   .parse(process.argv);
 
-if (program.new) {
-  frontpress.new(program.new);
+if (project.new) {
+  app.checkNameIsUnique(project.new)
+    .then(isUnique => {
+      if (isUnique) {
+        app.new(project.new);
+        process.exit();
+      }
+
+      return askWillReplacePath();
+    })
+    .then(answer => {
+      if (answer.replace) {
+        return app.new(project.new);
+      }
+
+      throw new Error(`Couldn't create the project path. Please, try again.`);
+    })
+    .catch(err => console.log(err.message));
 }
 
-if (program.init) {
+if (project.init) {
   inquirer
     .prompt(questions.init)
-    .then(answers => frontpress.init(answers));
+    .then(answers => app.init(answers));
+}
+
+function askWillReplacePath() {
+  return inquirer.prompt(questions.replaceProject);
 }
